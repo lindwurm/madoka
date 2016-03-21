fd33961cf05890ea69438ee63b1c999763a2ae40
lindwurm <admin@lindwurm.biz>
2016-03-21 10:21:58 +0900

    release

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# mashiroBuild
+# madoka
+
+主にカスタムROMのビルド実行用スクリプトです。  
+優秀なマネージャーのつもりですが、セットアップまではしてくれません。
+
+## 機能
+
+- 実行時の引数によるROM選択・ビルドターゲット・ツイート可否・ `repo sync` 可否・ `make clean` 可否 の指定
+    - 対話型でないので一度実行すれば終了まで操作不要
+- 開始/終了時にTwitterへ投稿
+    - [oysttyer](https://github.com/oysttyer/oysttyer) に丸投げしています。
+    - 各自で使う際はハッシュタグとか変えといてください
+- ログの保存、ビルド成否による保存先の振り分け
+- 複数種類のカスタムROMに対応可能
+    - ファイル名のフォーマット等、対応は各自で
+    - とりあえずCyanogenModとResurrection RemixとAOKPのフォーマットに対応しています
+        - が、バージョンベタ打ちなのが欠点
+- 終了時に [Pushbullet](https://www.pushbullet.com/) APIを使用したプッシュ通知
+    - アクセストークンの発行が必要です
+- ビルド完了時に [MEGA](https://mega.nz) へのアップロード
+    - MEGAのアカウント及び別途 [megatools](https://megatools.megous.com/) のセットアップが必要
+    - あとたぶんMEGA上でフォルダ作っとかないとコケるかも？
+- ビルド完了後に別ディレクトリへROMの `.zip` を退避
+    - デフォルトでは `~/rom` になっています
+    - 連続で複数機種ビルドする際に毎回 `make clean` する運用も可能に
+
+## 用法
+
+```bash
+mkdir -p build
+cd build
+git clone https://github.com/lindwurm/mashiroBuild.git
+```
+
+- ディレクトリ構造こんな感じになります
+    - `~/build` の名前はスクリプトに関係しませんし、わたしは実際のところ `~/ssd` にしていて、この実態は `/ssd1` にマウントされたSSDだったりします。)
+
+```
+~/
+|
+|-- build/
+|   |-- aokp/
+|   |-- cm13/
+|   |-- log/
+|   |   |-- fail/
+|   |   `-- success/
+|   |-- madoka/
+|   |   |-- build.sh
+|   |   |-- LICENSE
+|   |   `-- README.md
+|   `-- rr/
+`-- rom/  
+    `-- ${device}/
+```
+
+実際の使い方は以下の通りです。可否は0(否)か1(可)かで指定してください。
+
+```bash
+./build.sh [ROMのディレクトリ名] [ビルドターゲット] <ツイート可否> <repo sync可否> <make clean可否>
+```
+
+## ライセンス
+
+**madoka** は The MIT License で提供されます。
+
+## 作者
+
+- lindwurm
+    - Twitter: [@lindwurm](https://twitter.com/lindwurm)
+    - GitHub: [@lindwurm](https://github.com/lindwurm)
+    - Web: [maud.io](https://maud.io)
+    - Blog: [dev:mordiford](http://dev.maud.io)
---
+++ build.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# YOUR_ACCESS_TOKEN には https://www.pushbullet.com/#settings/account から取得したトークンを使用
+PUSHBULLET_TOKEN=YOUR_ACCESS_TOKEN
+
+# 実行時の引数が正しいかチェック
+if [ $# -ne 5 ]; then
+	echo "指定された引数は$#個です。" 1>&2
+	echo "仕様: $CMDNAME [ビルドディレクトリ] [ターゲット] [ツイート可否] [repo sync可否] [make clean可否]" 1>&2
+	echo "ツイート、repo sync、make cleanの可否は1(有効)か0(無効)で選択してください。" 1>&2
+	echo "ログは自動的に記録されます。" 1>&2
+	exit 1
+fi
+
+builddir=$1
+device=$2
+
+cd $builddir
+
+# repo sync
+if [ $4 -eq 1 ]; then
+	repo sync -j8 --force-sync
+	echo -e "\n"
+fi
+
+# make clean
+if [ $5 -eq 1 ]; then
+	make clean
+	echo -e "\n"
+fi
+
+# 現在日時取得、ログのファイル名設定
+starttime=$(date '+%Y/%m/%d %T')
+filetime=$(date '+%Y-%m-%d_%H-%M-%S')
+filename="${filetime}_${builddir}_${device}.log"
+
+# CMやRRの場合、吐き出すzipのファイル名はUTC基準での日付なので注意。
+zipdate=$(date -u '+%Y%m%d')
+# AOKPは他と違う上にホストのタイムゾーン基準での日時です
+aokpdate=$(date '+%Y-%m-%d_%H%M')
+
+# ディレクトリ名からツイート用のROM情報の設定とzipのファイル名当てをする
+if [ $builddir = cm13 ]; then
+	source="CyanogenMod 13.0"
+	zipname="cm-13.0-${zipdate}-UNOFFICIAL-$device"
+elif [ $builddir = rr ]; then
+	source="ResurrectionRemix v5.6.5"
+	zipname="ResurrectionRemix-M-v5.6.5-${zipdate}-$device"
+elif [ $builddir = aokp ]; then
+	source="AOKP (Marshmallow)"
+	zipname="aokp_${device}_mm_unofficial_${aokpdate}"
+else
+	source=$builddir
+	zipname="*"
+fi
+
+# 開始時のツイート
+if [ $3 -eq 1 ]; then
+	twstart=$(echo -e "${device} 向け ${source} のビルドを開始します。 \n\n$starttime #mashiroBuild")
+	perl ~/oysttyer/oysttyer.pl -ssl -status="$twstart"
+fi
+
+# ビルド
+source build/envsetup.sh
+
+echo -e "\n"
+
+brunch ${device} 2>&1 | tee "../log/$filename"
+
+if [ $(echo ${PIPESTATUS[0]}) -eq 0 ]; then
+	ans=1
+	statusdir="success"
+else
+	ans=0
+	statusdir="fail"
+fi
+
+cd ..
+mv -v log/$filename log/$statusdir/
+
+echo -e "\n"
+
+# 結果のツイート
+	endstr=$(cat -v "log/$statusdir/$filename" | tail -n 3 | tr -d '\n' | cut -d "#" -f 5-5 | cut -c 2-)
+if [ $3 -eq 1 ]; then
+	endtime=$(date '+%Y/%m/%d %H:%M:%S')
+	if [ $ans -eq 1 ]; then
+		twfinish=$(echo -e "${zipname} のビルドに成功しました！\n\n$endstr\n\n$endtime #mashiroBuild")
+	else
+		twfinish=$(echo -e "${device} 向け ${source} のビルドに失敗しました…\n\n$endstr\n\n$endtime #mashiroBuild")
+	fi
+
+	perl ~/oysttyer/oysttyer.pl -ssl -status="$twfinish" -autosplit=cut
+fi
+
+# Pushbullet APIを使ってプッシュ通知も投げる。文言は適当に
+endpush="build for ${device} #mashiroBuild"
+curl -u ${PUSHBULLET_TOKEN}: -X POST \
+  https://api.pushbullet.com/v2/pushes \
+  --header "Content-Type: application/json" \
+  --data-binary "{\"type\": \"note\", \"title\": \"${endpush}\", \"body\": \"${endstr}\"}"
+
+echo -e "\n"
+
+# ビルドが成功してればMEGAに上げつつ ~/rom に移動しておく
+# megaput は https://megatools.megous.com から megatools をインストール、
+# man を参照の上 ~/.megarc にユーザ名とパスワードを記載して使用
+if [ $ans -eq 1 ]; then
+
+	# $device に該当するフォルダが無い場合に備えてアップロード先はファイル名ごと指定
+	megaput $builddir/out/target/product/$device/${zipname}.zip --path /Root/mashiro/$device/${zipname}.zip
+
+	mkdir -p ~/rom/$device
+
+	mv -v $builddir/out/target/product/$device/${zipname}.zip ~/rom/$device/
+	mv -v $builddir/out/target/product/$device/${zipname}.zip.md5sum ~/rom/$device/
+
+	echo -e "\n"
+
+fi
